681b4c23d0dbf153b918e0f7b3a351a4241136f8
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2023-01-17 16:52:31 -0500

    updated to handle multiple sports

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import requests
 import sqlite3
 import json
 import os
+from pprint import pprint
 
 # eventually these will be environment variables
 ENDPOINT: str = "http://127.0.0.1:3000/"
 
 REQUEST_INFO = {
-    "URL": "https://gvsulakers.com"
+    "URL": "https://gvsulakers.com",
+    "SPORT": "baseball,football,cross-country"
 }
 
 
@@ -34,7 +36,7 @@ def wipe(cur: sqlite3.Cursor):
         ).fetchall()
     )
     for table in tables:
-        cur.execute("DROP table %s;" % table)
+        cur.execute("DROP table %s;" % table.replace("-", "_"))
 
 
 def config_database(response: dict):
@@ -42,10 +44,13 @@ def config_database(response: dict):
     db = sqlite3.connect(db_path)
     cur = db.cursor()
     wipe(cur)
-    for year in response.keys():
-        cur.execute(
-            f"""CREATE TABLE baseball_{year} (key TEXT, player_name TEXT);"""
-        )
+    for sport in response:
+        years = response[sport].keys()
+        sport = sport.replace("-", "_")
+        for year in years:
+            cur.execute(
+                "CREATE TABLE '{}' (key TEXT, player_name TEXT);""".format(sport + "_" + year)
+            )
     return db, cur
 
 
@@ -59,26 +64,29 @@ def column_exists(cur: sqlite3.Cursor, col_name: str, tbl_name: str) -> bool:
 
 
 def add_players(cur: sqlite3.Cursor, data):
-    for year, players in data.items():
-        for player_name, info in players.items():
-            cur.execute(
-                "INSERT OR IGNORE INTO baseball_{} (key, player_name) VALUES(?, ?)".format(
-                    year),
-                (info["number"], player_name)
-            )
-            for col, val in info.items():
-                if not column_exists(cur, col, f"baseball_{year}"):
-                    cur.execute(
-                        f"ALTER TABLE baseball_{year} ADD COLUMN '{col}' TEXT"
-                    )
+    for sport, yr in data.items():
+        sport = sport.replace("-", "_")
+        for year, players in yr.items():
+            for player_name, info in players.items():
                 cur.execute(
-                    f"UPDATE baseball_{year} SET '{col}' = ? WHERE key = ?",
-                    (val, info["number"])
+                    "INSERT OR IGNORE INTO {}_{} (key, player_name) VALUES(?, ?)".format(
+                        sport, year),
+                    (info["number"], player_name)
                 )
+                for col, val in info.items():
+                    if not column_exists(cur=cur, col_name=col, tbl_name=f"{sport}_{year}"):
+                        cur.execute(
+                            f"ALTER TABLE {sport}_{year} ADD COLUMN '{col}' TEXT"
+                        )
+                    cur.execute(
+                        f"UPDATE {sport}_{year} SET '{col}' = ? WHERE key = ?",
+                        (val, info["number"])
+                    )
 
 
 def main():
     r = retrieve()
+    pprint(r)
     db, cur = config_database(r)
     add_players(cur, r)
     db.commit()
